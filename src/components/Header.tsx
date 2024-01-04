fad4f707d75ba758898a186e76640418079b9267
alphakahere <alphakahere@gmail.com>
2024-01-04 00:58:43 +0000

    fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,15 +18,15 @@ const Header = () => {
 							JavaScript, TypeScript and PHP
 						</h1>
 						<p className="text-center">
-							I'm a full stack developer (React.js &
-							Node.js) with a focus on creating (and
-							occasionally designing) exceptional
-							digital experiences that are fast,
-							accessible, visually appealing, and
-							responsive. Even though I have been
-							creating web applications for over 2
-							years, I still love it as if it was
-							something new.
+							I&apos;m a full stack developer
+							(React.js & Node.js) with a focus on
+							creating (and occasionally designing)
+							exceptional digital experiences that
+							are fast, accessible, visually
+							appealing, and responsive. Even though
+							I have been creating web applications
+							for over 2 years, I still love it as
+							if it was something new.
 						</p>
 					</div>
 					<button className="bg-primary text-xs font-bold flex items-center gap-3 text-grey_light px-5 py-2 rounded-4xl -tracking-tighter uppercase">
