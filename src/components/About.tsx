79f008f1f492e1c920e0fe622d96506969c46052
alphakahere <alphakahere@gmail.com>
2024-03-04 14:59:36 +0000

    about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,32 +1,22 @@
 import { syne } from "@/constants/constants";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import React from "react";
 
 const About = () => {
+	const t = useTranslations("about");
 	return (
 		<section className="flex flex-col lg:flex-row items-center mb-24 relative">
 			<div className="lg:w-1/2">
 				<h3
 					className={`${syne.className} title-text text-4xl leading-snug mb-5`}
 				>
-					Let’s get know <br /> about me closer
+					{t("title")}
 				</h3>
-				<p className="text mb-12">
-					Aaronn is a New York-based visual designer
-					focusing on branding and visual identity. Her
-					portfolio showcases her wide range of work,
-					spanning books, posters and web design. Aaronn is
-					a New York-based visual designer focusing on
-					branding and visual identity. Her portfolio
-					showcases her wide range of work, spanning books,
-					posters and web design. Aaronn is a New York-based
-					visual designer focusing on branding and visual
-					identity. Her portfolio showcases her wide range
-					of work, spanning books, posters and web design.
-				</p>
-				<button className="button">
+				<p className="text mb-12">{t("text")}</p>
+				{/* <button className="button">
 					Discover More About Me
-				</button>
+				</button> */}
 			</div>
 			<div className="lg:w-1/2 flex justify-end">
 				<div className="relative">
@@ -37,8 +27,6 @@ const About = () => {
 						alt="photo profil"
 						className="rounded-4xl filter grayscale hover:filter-none transition-all ease-in duration-300"
 					/>
-					<div className="w-14 h-44 border-1 border-primary rounded-4xl absolute -left-10 bottom-5" />
-					<div className="w-10 h-32 border-1 border-primary rounded-4xl absolute right-10 -top-16 rotate-90" />
 				</div>
 			</div>
 			<div className="w-32 h-20 bg-grey inset-0 absolute -left-14 top-5 filter blur-3xl" />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,17 +20,6 @@ const Header = () => {
 						>
 							{t("title")}
 						</h1>
-						{/* <p className="text-center">
-							I&apos;m a full stack developer
-							(React.js & Laravel) with a focus on
-							creating (and occasionally designing)
-							exceptional digital experiences that
-							are fast, accessible, visually
-							appealing, and responsive. Even though
-							I have been creating web applications
-							for over 2 years, I still love it as
-							if it was something new.
-						</p> */}
 					</div>
 					<button className="bg-primary text-xs font-bold flex items-center gap-3 text-grey_light px-5 py-2 rounded-4xl -tracking-tighter uppercase">
 						<span>{t("buttonText")}</span>
