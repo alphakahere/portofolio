0caaf94b4a36b222df760b65205f21911c41945c
alphakahere <alphakahere@gmail.com>
2024-03-13 19:05:11 +0000

    technologies

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -4,6 +4,7 @@ import react from "../../public/images/react.svg";
 import Image from "next/image";
 import data from "../../messages/fr.json";
 import { useTranslations } from "next-intl";
+import { syne } from "@/constants/constants";
 
 const Technologies = () => {
 	const t = useTranslations("technologie");
@@ -16,19 +17,22 @@ const Technologies = () => {
 						className="flex items-center rounded-lg gap-4 mb-3 bg-dark3 p-3"
 						key={i}
 					>
-						<div className="bg-dark flex justify-center  items-center p-3 rounded-lg">
+						<div className="bg-dark flex justify-center  items-center p-3 rounded-lg w-44 h-24  min-w-[100px]">
 							<Image
-								src={react}
+								src={item.image}
 								alt="react-icon"
 								width={150}
 								height={100}
+								// className="w-20 h-20"
 							/>
 						</div>
 						<div>
-							<h3 className="text-lg font-semibold text-white mb-1">
+							<h3
+								className={`${syne.className} text-xl font-semibold text-white mb-1`}
+							>
 								{t(`list.${i}.name`)}
 							</h3>
-							<p className="text-sm text-white opacity-80">
+							<p className="text-base text-white opacity-80">
 								{t(`list.${i}.description`)}
 							</p>
 						</div>
